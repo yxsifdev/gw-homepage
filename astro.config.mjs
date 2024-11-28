935a680dc528b50dd433a3e63dbe5efdf6ec2707
yxsifdev <yosifblancotafur2006@gmail.com>
2024-11-28 16:12:14 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://yxsifdev.github.io",
-  base: "gw-homepage",
+  // base: "gw-homepage",
   integrations: [tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#272934",
+        secondary: "#1f2129",
+      },
+    },
+  },
+  plugins: [],
+};
